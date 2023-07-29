6a2fa0e7c5ec9560a25b17568292033b48283308
Gustav Modishi <mamosadi.gustav@gmail.com>
2023-07-29 21:25:48 +0200

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import {FaFreeCodeCamp} from 'react-icons/fa';
+
 
 function App() {
+
+  const initial = `
+  # Welcome to my React Markdown Previewer!
+
+  ## This is a sub-heading...
+  ### And here's some other cool stuff:
+  
+  Heres some code, \`<div></div>\`, between 2 backticks.
+  
+  \`\`\`
+  // this is multi-line code:
+  
+  function anotherExample(firstLine, lastLine) {
+    if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+      return multiLineCode;
+    }
+  }
+  \`\`\`
+  
+  You can also make text **bold**... whoa!
+  Or _italic_.
+  Or... wait for it... **_both!_**
+  And feel free to go crazy ~~crossing stuff out~~.
+  
+  There's also [links](https://www.freecodecamp.org), and
+  > Block Quotes!
+  
+  And if you want to get really crazy, even tables:
+  
+  Wild Header | Crazy Header | Another Header?
+  ------------ | ------------- | -------------
+  Your content can | be here, and it | can be here....
+  And here. | Okay. | I think we get it.
+  
+  - And of course there are lists.
+    - Some are bulleted.
+      - With different indentation levels.
+          - That look like this.
+  
+  
+  1. And there are numbered lists too.
+  1. Use just 1s if you want!
+  1. And last but not least, let's not forget embedded images:
+  
+  ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)`
+
+
+  const [text, setText] = useState(initial)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="editor-wrap">
+      <div className="top-bar"><span><FaFreeCodeCamp />Editor</span></div>
+      <textarea 
+      id="editor"
+      value={text}
+      onChange={(e) => setText(e.target.value)} 
+      >  
+      </textarea>
+      </div>
+      <div className="preview-wrap">
+      <div className="top-bar"><span><FaFreeCodeCamp />Previewer</span></div>
+      <div id="preview">
+    
+        <ReactMarkdown>{text}</ReactMarkdown>
+      </div>
+      </div>
     </div>
   );
 }
